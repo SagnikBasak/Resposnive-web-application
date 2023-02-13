23e8f159c83c20a83c61fa11226c1f2f3242c3c8
Sagnik Basak <110663497+SagnikBasak@users.noreply.github.com>
2023-02-13 22:30:18 +0530

    Add files via upload

---
+++ Reg_user_validation.js
@@ -0,0 +1,120 @@
+function validation(){
+  var name = document.getElementById('fullname').value;
+  var email = document.getElementById('email').value;
+  var mobileNumber = document.getElementById('phone').value;
+//  var pass = document.getElementById('pass').value;
+//  var conpass = document.getElementById('conpass').value;
+
+//For name field
+  if(name == ""){
+    document.getElementById('username').innerHTML ="** Please fill the Full Name field";
+    return false;
+  }
+  if((name.length <= 2) || (name.length >= 20)) {
+    document.getElementById('username').innerHTML ="** Username lenght must be between 2 and 20";
+    return false;
+  }
+  if(!isNaN(name)){
+    document.getElementById('username').innerHTML ="** Only characters are allowed";
+    return false;
+  }
+
+//For email field
+  if(email == ""){
+    document.getElementById('emailid').innerHTML ="** Please fill the email id field";
+    return false;
+  }
+  if(email.indexOf('@') <= 0 ){
+    document.getElementById('emailid').innerHTML ="** @ Invalid Position";
+    return false;
+  }
+  if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
+    document.getElementById('emailid').innerHTML ="** Invalid Position";
+    return false;
+  }
+
+//For mobile number field
+  if(mobileNumber == ""){
+    document.getElementById('mobileno').innerHTML ="** Please fill the mobile Number";
+    return false;
+  }
+  if(isNaN(mobileNumber)){
+    document.getElementById('mobileno').innerHTML ="** user must enter digits only not characters";
+    return false;
+  }
+  if(mobileNumber.length!=10){
+    document.getElementById('mobileno').innerHTML ="** Mobile Number must be 10 digits only";
+    return false;
+  }
+
+/*//For Password & Confirm password field
+  if(pass == ""){
+    document.getElementById('passwords').innerHTML ="** Please fill the password";
+    return false;
+  }
+  if(pass!=confirmpass){
+    document.getElementById('confrmpass').innerHTML ="** Password does not match the confirm password";
+    return false;
+  }
+  if(confirmpass == ""){
+    document.getElementById('confrmpass').innerHTML ="** Please fill the confirm password field";
+    return false;
+  }*/
+}
+
+function pass_check()
+{
+    var pass = document.getElementById("pass").value;
+
+    pass=pass.trim();
+    document.getElementById("pass").value=pass;
+    if(pass.length>=5)
+    {
+        document.getElementById("check0").style.color="green";
+        document.getElementById("check0").style.transitionDuration = "1s";
+    }
+    else
+    {
+       document.getElementById("check0").style.color="red";
+    }
+
+    if(pass.length<=10)
+    {
+        document.getElementById("check1").style.color="green";
+        document.getElementById("check1").style.transitionDuration = "1s";
+    }
+    else
+    {
+       document.getElementById("check1").style.color="red";
+    }
+
+    if(pass.match(/[0-9]/i))
+    {
+        document.getElementById("check2").style.color="green";
+        document.getElementById("check2").style.transitionDuration = "1s";
+    }
+    else
+    {
+       document.getElementById("check2").style.color="red";
+    }
+
+    if(pass.match(/[^A-Za-z0-9-' ']/i))
+    {
+        document.getElementById("check3").style.color="green";
+        document.getElementById("check3").style.transitionDuration = "1s";
+    }
+    else
+    {
+       document.getElementById("check3").style.color="red";
+    }
+
+    if(pass.match(' '))
+    {
+        document.getElementById("check4").style.color="red";
+    }
+    else
+    {
+       document.getElementById("check4").style.color="green";
+       document.getElementById("check4").style.transitionDuration = "1s";
+    }
+}
---
+++ registration_connect.php
@@ -0,0 +1,101 @@
+<?php
+
+function getIp() {
+  if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+  else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+  return $ip;
+}
+
+
+function getOS() {
+  $u_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+  $operating_system = 'Unknown Operating System';
+  if($u_agent) {
+    if (preg_match('/linux/i', $u_agent)) {
+      $operating_system = 'Linux';
+    } elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
+      $operating_system = 'Mac';
+    } elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
+      $operating_system = 'Windows';
+    } elseif (preg_match('/ubuntu/i', $u_agent)) {
+      $operating_system = 'Ubuntu';
+    } elseif (preg_match('/iphone/i', $u_agent)) {
+      $operating_system = 'IPhone';
+    } elseif (preg_match('/ipod/i', $u_agent)) {
+      $operating_system = 'IPod';
+    } elseif (preg_match('/ipad/i', $u_agent)) {
+      $operating_system = 'IPad';
+    } elseif (preg_match('/android/i', $u_agent)) {
+      $operating_system = 'Android';
+    } elseif (preg_match('/blackberry/i', $u_agent)) {
+      $operating_system = 'Blackberry';
+    } elseif (preg_match('/webos/i', $u_agent)) {
+      $operating_system = 'Mobile';
+    }
+  } else {
+    $operating_system = php_uname('s');
+  }
+    return $operating_system;
+}
+
+
+function getBrowser() {
+  $arr_browsers = ["Opera", "Edg", "Chrome", "Safari", "Firefox", "MSIE", "Trident"];
+  $agent = $_SERVER['HTTP_USER_AGENT'];
+  $user_browser = '';
+  foreach ($arr_browsers as $browser) {
+      if (strpos($agent, $browser) !== false) {
+          $user_browser = $browser;
+          break;
+      }
+  }
+  switch ($user_browser) {
+      case 'MSIE':
+          $user_browser = 'Internet Explorer';
+          break;
+
+      case 'Trident':
+          $user_browser = 'Internet Explorer';
+          break;
+
+      case 'Edg':
+          $user_browser = 'Microsoft Edge';
+          break;
+  }
+      return $user_browser;
+}
+
+
+  $fullName = $_POST['fullName'];
+  $email = $_POST['email'];
+  $number = $_POST['number'];
+  $dob = date('y-m-d', strtotime($_POST['dob']));
+  $password = $_POST['password'];
+  $gender = $_POST['gender'];
+  $ip = getIp();
+  $os = getOS();
+  $browser = getBrowser();
+
+  // Database connection
+  $con=mysqli_connect("localhost","root","","accounts");
+  //print_r($con);
+  if ($con->connect_error) {
+    echo "Connection Failed";
+  }
+  else {
+    $sql="insert into registration(fullName, email, number, dob, password, gender, ip, os, browser)
+    values('$fullName', '$email', $number, '$dob', '$password', '$gender', '$ip', '$os', '$browser')";
+    $con->query($sql);
+    echo "Registration Successful";
+  }
+?>
